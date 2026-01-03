25cae83ec6007e9d592baad73395fea932cbe67f
suma052 <it24052@mbstu.ac.bd>
2026-01-04 00:10:08 +0600

    Create Account class with deposit and balance methods

---
+++ Create Account.java
@@ -0,0 +1,11 @@
+public class Account {
+    private double balance;
+
+    public void deposit(double amount){
+        if(amount > 0) balance += amount;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+}
